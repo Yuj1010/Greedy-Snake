32efecea54de0c79cb22b53e0e50081341f4f537
Yuj1010 <41867171+Yuj1010@users.noreply.github.com>
2020-06-16 15:18:20 +0800

    Update Main.c

--- Greedy Snake/贪吃蛇/Main.c
+++ Greedy Snake/贪吃蛇/Main.c
@@ -4,17 +4,10 @@
 
 int main()
 {
-	printf("ª∂”≠\n");
+	printf("Welcome\n");
 	system("pause");
 	system("cls");
 
-	printf("ª∂”≠\n");
-	system("pause");
-	system("cls");
-
-	printf("»»¡“ª∂”≠\n");
-	system("pause");
-	system("cls");
 	GameStart();
 
 	return 0;
